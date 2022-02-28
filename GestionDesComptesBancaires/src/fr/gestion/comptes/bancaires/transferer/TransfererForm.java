7b9edb297e279d0abaffcf301b1fbd02e260d6b0
maya22ch <chbibsoumaya@gmail.com>
2022-02-28 21:26:52 +0100

    Interface modifier et transferer

---
+++ GestionDesComptesBancaires/src/fr/gestion/comptes/bancaires/transferer/TransfererForm.java
@@ -0,0 +1,43 @@
+package fr.gestion.comptes.bancaires.transferer;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class TransfererForm {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TransfererForm window = new TransfererForm();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TransfererForm() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
