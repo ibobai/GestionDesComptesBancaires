b1732da5da80566486b6115441c68cfec6b2057f
Naoual BECHIR <n.bechir@hotmail.fr>
2022-03-01 16:21:17 +0100

    interface ok

--- GestionDesComptesBancaires/src/fr/gestion/comptes/bancaires/comptes/ListeComptesForm.java
+++ GestionDesComptesBancaires/src/fr/gestion/comptes/bancaires/comptes/ListeComptesForm.java
@@ -9,28 +9,28 @@ import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.table.DefaultTableModel;
 
 public class ListeComptesForm {
 
 	private JFrame frame;
-	private JTextField txtIsnBank;
 	private JTextField txtGestionDeLa;
-	private JTextField txtTypeDeCompte;
-	private JTextField txtNumeroDeCompte;
-	private JTextField txtLibellClient;
-	private JTextField txtSole;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
-	private JTextField textField_9;
-	private JTextField textField_10;
-	private JTextField textField_11;
+	private JTable table;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
 	private JButton btnNewButton_3;
-	private JButton btnNewButton_4;
+	private JButton btnOuvdbiter;
 	private JButton btnNewButton_5;
 	private JButton btnNewButton_6;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -61,139 +61,140 @@ public class ListeComptesForm {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(190, 247, 251));
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		txtIsnBank = new JTextField();
-		txtIsnBank.setBackground(new Color(190, 247, 251));
-		txtIsnBank.setText("ISN BANK");
-		txtIsnBank.setBounds(0, 0, 86, 20);
-		frame.getContentPane().add(txtIsnBank);
-		txtIsnBank.setColumns(10);
 		
 		txtGestionDeLa = new JTextField();
+		txtGestionDeLa.setBackground(new Color(118, 199, 240));
 		txtGestionDeLa.setHorizontalAlignment(SwingConstants.CENTER);
-		txtGestionDeLa.setText("Gestion de la liste des compte");
-		txtGestionDeLa.setBounds(124, 11, 252, 42);
-		frame.getContentPane().add(txtGestionDeLa);
+		txtGestionDeLa.setFont(new Font("Verdana", Font.BOLD, 30));
+		txtGestionDeLa.setText("Gestion de la liste des comptes");
 		txtGestionDeLa.setColumns(10);
 		
-		txtTypeDeCompte = new JTextField();
-		txtTypeDeCompte.setText("Type de compte");
-		txtTypeDeCompte.setHorizontalAlignment(SwingConstants.CENTER);
-		txtTypeDeCompte.setBounds(0, 64, 86, 20);
-		frame.getContentPane().add(txtTypeDeCompte);
-		txtTypeDeCompte.setColumns(10);
+		table = new JTable();
+		table.setColumnSelectionAllowed(true);
+		table.setFont(new Font("Verdana", Font.PLAIN, 15));
+		table.setBackground(new Color(118, 199, 240));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Type de compte", "Numero de compte", "Lib\u00E9ll\u00E9 Client", "Solde"},
+				{"", "", "", null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(154);
+		table.getColumnModel().getColumn(1).setPreferredWidth(154);
+		table.getColumnModel().getColumn(2).setPreferredWidth(154);
+		table.getColumnModel().getColumn(3).setPreferredWidth(154);
 		
-		txtNumeroDeCompte = new JTextField();
-		txtNumeroDeCompte.setText("Numero de compte");
-		txtNumeroDeCompte.setHorizontalAlignment(SwingConstants.CENTER);
-		txtNumeroDeCompte.setBounds(96, 64, 104, 20);
-		frame.getContentPane().add(txtNumeroDeCompte);
-		txtNumeroDeCompte.setColumns(10);
+		btnNewButton = new JButton("Modifier");
+		btnNewButton.setFont(new Font("Verdana", Font.PLAIN, 15));
+		btnNewButton.setBackground(new Color(131, 224, 229));
 		
-		txtLibellClient = new JTextField();
-		txtLibellClient.setText("Libell\u00E9 client");
-		txtLibellClient.setHorizontalAlignment(SwingConstants.CENTER);
-		txtLibellClient.setBounds(220, 64, 86, 20);
-		frame.getContentPane().add(txtLibellClient);
-		txtLibellClient.setColumns(10);
+		btnNewButton_1 = new JButton("Cr\u00E9diter");
+		btnNewButton_1.setFont(new Font("Verdana", Font.PLAIN, 15));
+		btnNewButton_1.setBackground(new Color(131, 224, 229));
 		
-		txtSole = new JTextField();
-		txtSole.setText("Sole");
-		txtSole.setHorizontalAlignment(SwingConstants.CENTER);
-		txtSole.setBounds(326, 64, 86, 20);
-		frame.getContentPane().add(txtSole);
-		txtSole.setColumns(10);
+		btnNewButton_2 = new JButton("Transf\u00E9rer");
+		btnNewButton_2.setFont(new Font("Verdana", Font.PLAIN, 15));
+		btnNewButton_2.setBackground(new Color(131, 224, 229));
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(0, 95, 86, 20);
-		frame.getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		btnNewButton_3 = new JButton("Cloturer");
+		btnNewButton_3.setFont(new Font("Verdana", Font.PLAIN, 15));
+		btnNewButton_3.setBackground(new Color(131, 224, 229));
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(96, 95, 104, 20);
-		frame.getContentPane().add(textField_5);
-		textField_5.setColumns(10);
-		
-		textField_6 = new JTextField();
-		textField_6.setBounds(220, 95, 86, 20);
-		frame.getContentPane().add(textField_6);
-		textField_6.setColumns(10);
-		
-		textField_7 = new JTextField();
-		textField_7.setBounds(326, 95, 86, 20);
-		frame.getContentPane().add(textField_7);
-		textField_7.setColumns(10);
-		
-		textField_8 = new JTextField();
-		textField_8.setBounds(0, 126, 86, 20);
-		frame.getContentPane().add(textField_8);
-		textField_8.setColumns(10);
-		
-		textField_9 = new JTextField();
-		textField_9.setBounds(96, 126, 104, 20);
-		frame.getContentPane().add(textField_9);
-		textField_9.setColumns(10);
-		
-		textField_10 = new JTextField();
-		textField_10.setBounds(220, 126, 86, 20);
-		frame.getContentPane().add(textField_10);
-		textField_10.setColumns(10);
-		
-		textField_11 = new JTextField();
-		textField_11.setBounds(326, 126, 86, 20);
-		frame.getContentPane().add(textField_11);
-		textField_11.setColumns(10);
-		
-		JButton btnNewButton = new JButton("Ouvrir un compte");
-		btnNewButton.addActionListener(new ActionListener() {
+		btnOuvdbiter = new JButton("D\u00E9biter");
+		btnOuvdbiter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setToolTipText("");
-		btnNewButton.setBounds(10, 156, 116, 23);
-		frame.getContentPane().add(btnNewButton);
+		btnOuvdbiter.setFont(new Font("Verdana", Font.PLAIN, 15));
+		btnOuvdbiter.setBackground(new Color(131, 224, 229));
 		
-		JButton btnNewButton_1 = new JButton("Modifier");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_1.setBounds(180, 156, 89, 23);
-		frame.getContentPane().add(btnNewButton_1);
+		btnNewButton_5 = new JButton("Ouvrir un compte");
+		btnNewButton_5.setFont(new Font("Verdana", Font.PLAIN, 15));
+		btnNewButton_5.setBackground(new Color(131, 224, 229));
 		
-		JButton btnNewButton_2 = new JButton("D\u00E9biter");
-		btnNewButton_2.setBounds(10, 183, 116, 23);
-		frame.getContentPane().add(btnNewButton_2);
-		
-		btnNewButton_3 = new JButton("Crediter");
-		btnNewButton_3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_3.setBounds(180, 183, 89, 23);
-		frame.getContentPane().add(btnNewButton_3);
-		
-		btnNewButton_4 = new JButton("Tensf\u00E9rer");
-		btnNewButton_4.setBounds(323, 156, 89, 23);
-		frame.getContentPane().add(btnNewButton_4);
-		
-		btnNewButton_5 = new JButton("Cloturer");
-		btnNewButton_5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_5.setBounds(323, 183, 89, 23);
-		frame.getContentPane().add(btnNewButton_5);
-		
-		btnNewButton_6 = new JButton("Deconnexion");
+		btnNewButton_6 = new JButton("D\u00E9connexion");
 		btnNewButton_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_6.setBounds(162, 224, 136, 23);
-		frame.getContentPane().add(btnNewButton_6);
+		btnNewButton_6.setFont(new Font("Verdana", Font.PLAIN, 15));
+		btnNewButton_6.setBackground(new Color(118, 199, 240));
+		
+		lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(ListeComptesForm.class.getResource("/res/ISN_BANK_logo_sans_trais.png")));
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(344)
+					.addComponent(txtGestionDeLa, GroupLayout.PREFERRED_SIZE, 616, GroupLayout.PREFERRED_SIZE)
+					.addGap(336))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(185)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnNewButton_5, GroupLayout.PREFERRED_SIZE, 190, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 190, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnOuvdbiter, GroupLayout.PREFERRED_SIZE, 190, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 513, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 190, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 190, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 190, GroupLayout.PREFERRED_SIZE))
+					.addGap(218))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(1188, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(17)
+					.addComponent(table, GroupLayout.PREFERRED_SIZE, 1250, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(29, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(536)
+					.addComponent(btnNewButton_6, GroupLayout.PREFERRED_SIZE, 190, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(558, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
+					.addGap(43)
+					.addComponent(txtGestionDeLa, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+					.addGap(46)
+					.addComponent(table, GroupLayout.PREFERRED_SIZE, 149, GroupLayout.PREFERRED_SIZE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(26)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnNewButton_5, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE))
+							.addGap(27)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnOuvdbiter, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE))
+							.addGap(27)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE))
+							.addContainerGap(75, Short.MAX_VALUE))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnNewButton_6, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+							.addGap(26))))
+		);
+		frame.getContentPane().setLayout(groupLayout);
+		frame.setBounds(100, 100, 1300, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
